166c3a88b0481ed9aca29aa3b78fc5997ff99faa
Hansen, Barb A <Hansen.Barb.A@principal.com>
2021-03-04 06:09:51 -0600

    added code to DivideUpEightSlicePizzasEvenly to calculate and then return slicesPizzaPerPerson
    Added code for remainder - used % as operand
    
    Added interest rate calculation to return
    
    added BAC calculations

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,10 +1,10 @@
 public class BowlingGame {
-    // TODO: Why must this array have a size of 21?
+    // TODO: In case we have a strike at the end
     int[] rolls = new int[21];
     private int index;
 
     public void roll(int pins) {
-        // TODO: Replace this with description of why this postfix operator works here
+        // TODO: Adds 1 to index each time
         rolls[index++] = pins;
     }
 
@@ -13,17 +13,9 @@ public class BowlingGame {
         // TODO CODE: Change this to use a loop prior to implementing spare and strike calculation and then commit to git
         //            This change should not break the existing tests that pass. This is called refactoring.
 
-        // TODO RESPONSE: Why does the subscript need to start with zero?
-        score += rolls[0] + rolls[1];
-        score += rolls[2] + rolls[3];
-        score += rolls[4] + rolls[5];
-        score += rolls[6] + rolls[7];
-        score += rolls[8] + rolls[9];
-        score += rolls[10] + rolls[11];
-        score += rolls[12] + rolls[13];
-        score += rolls[14] + rolls[15];
-        score += rolls[16] + rolls[17];
-        score += rolls[18] + rolls[19];
+        // TODO RESPONSE: In java, indexes start with 0
+        for(int currentVal:rolls)
+            score += currentVal;
         return score;
     }
 
